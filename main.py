f0dd7bcd429780c6d9e5c0d46fd3ebe5b0855d15
phumiwit <phumiwit@gmail.com>
2022-12-08 20:46:25 +0700

    Add design file

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from key import Keyword_Spotting_Service
+import uvicorn
+app = FastAPI()
+
+@app.get('/')
+def Hello():
+    return {'Hello':'Hello'}
+
+@app.get('/predict')
+def predict_genre(path :str):
+    
+    kss = Keyword_Spotting_Service()
+    keyword1,keyword2= kss.prediction(path)
+    return {"prediction":keyword1}
+
+@app.get('/value')
+def value_genre(path : str):
+    kss = Keyword_Spotting_Service()
+    keyword1,keyword2 = kss.prediction(path)
+    
+    return keyword2
+
+
+if __name__ == "__main__":
+     uvicorn.run(app,host='127.0.0.1',port=8000)
