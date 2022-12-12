2ba38053a2bbd2690fa59d45f2fc3cf4d40ec896
phumiwit <68888298+phumiwit@users.noreply.github.com>
2022-12-12 20:28:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from key import Keyword_Spotting_Service
 import uvicorn
 app = FastAPI()
 
+
+
+
+    
 @app.get('/')
 def Hello():
     return {'Hello':'Hello'}
 
-@app.get('/predict')
-def predict_genre(path :str):
+@app.post('/predict')
+def predict_genre(path:str):
     
     kss = Keyword_Spotting_Service()
     keyword1,keyword2= kss.prediction(path)
     return {"prediction":keyword1}
 
-@app.get('/value')
+@app.post('/value',)
 def value_genre(path : str):
     kss = Keyword_Spotting_Service()
     keyword1,keyword2 = kss.prediction(path)
